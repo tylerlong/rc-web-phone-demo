b0a6ac55ac1f33eae1dcc0d65506f7f245d397d2
Tyler Liu <tyler4long@gmail.com>
2024-03-18 10:50:38 -0700

    Create store

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
-import { Button, Space, Typography } from 'antd';
+import { Input, Typography, Form } from 'antd';
 import { auto } from 'manate/react';
 
 import type { Store } from './store';
 
 const { Text, Title } = Typography;
 
+const Login = (props: { store: Store }) => {
+  const { store } = props;
+  const render = () => (
+    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+      <Form.Item label="Client ID">
+        <Input
+          onChange={(e) => {
+            store.clientId = e.target.value;
+          }}
+          value={store.clientId}
+        ></Input>
+      </Form.Item>
+      <Form.Item label="Client Secret">
+        <Input.Password
+          onChange={(e) => {
+            store.clientSecret = e.target.value;
+          }}
+          value={store.clientSecret}
+        ></Input.Password>
+      </Form.Item>
+    </Form>
+  );
+  return auto(render, props);
+};
+
+const Phone = (props: { store: Store }) => {
+  const { store } = props;
+  const render = () => <>Phone here</>;
+  return auto(render, props);
+};
+
 const App = (props: { store: Store }) => {
   const { store } = props;
   const render = () => (
     <>
       <Title>RingCentral Web Phone Demo</Title>
-      <Space>
-        <Button
-          onClick={() => {
-            store.count -= 1;
-          }}
-        >
-          -
-        </Button>
-        <Text>{store.count}</Text>
-        <Button
-          onClick={() => {
-            store.count += 1;
-          }}
-        >
-          +
-        </Button>
-      </Space>
+      {store.rcToken === '' ? <Login store={store} /> : <Phone store={store} />}
     </>
   );
   return auto(render, props);
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,50 @@
-import { manage } from 'manate';
+import { autoRun, manage } from 'manate';
+import RingCentral from '@rc-ex/core';
+import localForage from 'localforage';
 
 export class Store {
-  public count = 0;
+  public rcToken = '';
+  public refreshToken = '';
+  public clientId = '';
+  public clientSecret = '';
 }
 
 const store = manage(new Store());
+
+const main = async () => {
+  store.rcToken = (await localForage.getItem('rcToken')) ?? '';
+  store.refreshToken = (await localForage.getItem('refreshToken')) ?? '';
+  store.clientId = (await localForage.getItem('clientId')) ?? '';
+  store.clientSecret = (await localForage.getItem('clientSecret')) ?? '';
+
+  // auto save things to local
+  const { start } = autoRun(store, () => {
+    localForage.setItem('rcToken', store.rcToken);
+    localForage.setItem('refreshToken', store.refreshToken);
+    localForage.setItem('clientId', store.clientId);
+    localForage.setItem('clientSecret', store.clientSecret);
+  });
+  start();
+
+  // validate saved token
+  if (store.rcToken === '') {
+    return;
+  }
+  const rc = new RingCentral({
+    server: 'https://platform.ringcentral.com',
+  });
+  rc.token = { access_token: store.rcToken, refresh_token: store.refreshToken };
+  try {
+    await rc.restapi().account().extension().get();
+  } catch (e) {
+    try {
+      await rc.refresh();
+    } catch (e) {
+      store.rcToken = '';
+      store.refreshToken = '';
+    }
+  }
+};
+main();
 
 export default store;
